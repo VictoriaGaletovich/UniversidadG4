a80a5c1f37fb13c4239c9b68718b08061a7d393b
Veronica <135274833+Galbickus@users.noreply.github.com>
2023-09-14 23:54:40 -0300

    Agrega clase InscripcionData.
    InscripcionData

--- UniversidadTest/src/accesoADatos/InscripcionData.java
+++ UniversidadTest/src/accesoADatos/InscripcionData.java
@@ -1,57 +1,172 @@
+
 package accesoadatos;
 
+import entidades.Alumno;
 import entidades.Inscripcion;
+import entidades.Materia;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
+
 
 /**
  * @author Veronica
  */
 public class InscripcionData {
+  private Connection conexion = null;
+  private AlumnoData ad = new AlumnoData();
+  private MateriaData md = new MateriaData();
+  
+/* se declara una variable privada y 
+  se inicializa creando una nueva instancia de la clase AlumnoData por esto
+  la clase InscripcionData tiene una dependencia de la clase AlumnoData 
+  y puede utilizar sus métodos y propiedades a través de esta variable ad.*/  
 
-    private Connection con = null;
-    private AlumnoData alumnoData;
-    private MateriaData materiaData;
 
-    //pruebo agregar este constructor vacio para poder hacer las pruebas desde el main
+
     public InscripcionData() {
+        this.conexion = Conexion.getConnection();
     }
+    
+    
 
-    public InscripcionData(Connection con, AlumnoData alumnoData, MateriaData materiaData) {
-        this.con = Conexion.getConexion();
-        this.alumnoData = alumnoData;
-        this.materiaData = materiaData;
-    }
-
-    public void guardarInscripcion(Inscripcion inscripcion) {
-
-        String sql = "INSERT INTO inscripcion(idAlumno, idMmateria, nota)VALUES(?,?,?)";
-
+//    public InscripcionData(Connection conexion, AlumnoData alumnoData, MateriaData materiaData) {
+//        this.conexion = Conexion.getConnection();
+//        this.alumnoData = alumnoData;
+//        this.materiaData = materiaData;
+//    }
+    
+    
+    public void guardarInscripcion(Inscripcion inscripcion){
+        
+        String sql = "INSERT INTO inscripcion(idAlumno, idMateria, nota)VALUES(?,?,?)"; 
+        
         try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, inscripcion.getAlumno().getIdAlumno());
-            ps.setInt(2, inscripcion.getMateria().getIdMateria());
-            ps.setDouble(3, inscripcion.getNota());
+            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1,inscripcion.getAlumno().getIdAlumno());
+            ps.setInt(2,inscripcion.getMateria().getIdMateria());
+            ps.setDouble(3,inscripcion.getNota());
             ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
-
-            if (rs.next()) {
-                inscripcion.setIdInscripcion(rs.getInt(1));
-                JOptionPane.showMessageDialog(null, "Se inscribió a " + inscripcion.getMateria().getNombre());
-            } else {
-                JOptionPane.showMessageDialog(null, "No se puedo inscribir");
+            
+            if(rs.next()){
+                inscripcion.setIdInscripcion(rs.getInt(1)); //el 1 es porque quiero el idInscripcion que está en la columna 1 
+                JOptionPane.showMessageDialog(null, "Inscripción realizada");
+            }else{
+                JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla inscribir");
             }
             ps.close();
-
+            
+          
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "No se pudo inscribir " + ex.getMessage());
-
+            JOptionPane.showMessageDialog(null, "No se pudo inscribir "+ ex.getMessage());
+    
         }
+        
+    }
+    
+    public void actualizarNota(double nota, int idAlumno, int idMateria){
+        
+        String sql = "UPDATE inscripcion SET nota = ? WHERE idAlumno = ? AND idMateria = ?";
+      try {
+          PreparedStatement ps = conexion.prepareStatement(sql);
+          ps.setDouble(1, nota);
+          ps.setInt(2, idAlumno);
+          ps.setInt(3, idMateria);
+          int fila = ps.executeUpdate();
+          if(fila>0){
+          JOptionPane.showMessageDialog(null, "Nota actualizada");
+  
+          }
+          ps.close();
+      } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "No se pudo actualizar nota "+ ex.getMessage());
 
+      }
+        
+        
+    }
+    
+    public void borrarInscripcionMateriaAlumno(int idAlumno,int idMateria){
+        
+        String sql = "DELETE FROM inscripcion WHERE idAlumno = ? AND idMateria = ?";
+      try {
+          PreparedStatement ps = conexion.prepareStatement(sql);
+          ps.setInt(1, idAlumno);
+          ps.setInt(2, idMateria);
+          int filas = ps.executeUpdate();
+          if(filas>0){
+          JOptionPane.showMessageDialog(null, "Inscripcion borrada");
+          }
+          ps.close();
+      } catch (SQLException ex) {
+          JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla inscripcion");
+      }
+        
     }
 
+    /*en SELECT usamos executeQuery que no devuelve un entero, 
+    devuelve un resultSet que es como una matriz
+    conexion tantas  columnas como columna tenga la tabla 
+    y tantas filas como elementos tenga 
+    ( en este caso elementos = inscripciones)*/
+    
+    public List<Inscripcion> obtenerInscripciones(){
+        ArrayList<Inscripcion> cursadas = new ArrayList<>();
+        String sql = "SELECT * FROM inscripcion";
+      try {
+          PreparedStatement ps = conexion.prepareStatement(sql);
+          ResultSet rs = ps.executeQuery(); 
+          
+          while(rs.next()){
+              
+              Inscripcion inscripcion = new Inscripcion();
+              inscripcion.setIdInscripcion(rs.getInt("idInscripcion"));
+              Alumno alumno = ad.buscarAlumnoPorID( rs.getInt("idAlumno"));
+              Materia materia = md.buscarMateriaPorID(rs.getInt("idMateria"));
+              inscripcion.setAlumno(alumno);
+              inscripcion.setMateria(materia);
+              inscripcion.setNota(rs.getDouble("nota"));
+              cursadas.add(inscripcion);
+              }
+          ps.close();
+      } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla inscripcion");
+
+      }
+      return cursadas;
+    }
+    
+        public List<Inscripcion> obtenerInscripcionesPorAlumno(int idAlumno){
+        ArrayList<Inscripcion> inscripcionPorAlumno = new ArrayList<>();
+        String sql = "SELECT * FROM inscripcion WHERE idAlumno = ?";
+      try {
+          PreparedStatement ps = conexion.prepareStatement(sql);
+          ps.setInt(1, idAlumno);
+          ResultSet rs = ps.executeQuery(); 
+          
+          while(rs.next()){
+              
+              Inscripcion inscripcion = new Inscripcion();
+              inscripcion.setIdInscripcion(rs.getInt("idInscripcion"));
+              Alumno alumno = ad.buscarAlumnoPorID( rs.getInt("idAlumno"));
+              Materia materia = md.buscarMateriaPorID(rs.getInt("idMateria"));
+              inscripcion.setAlumno(alumno);
+              inscripcion.setMateria(materia);
+              inscripcion.setNota(rs.getDouble("nota"));
+              inscripcionPorAlumno.add(inscripcion);
+              }
+          ps.close();
+      } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla inscripcion");
+
+      }
+      return inscripcionPorAlumno;
+    }
+    
 }
--- UniversidadTest/src/universidadtest/UniversidadTest.java
+++ UniversidadTest/src/universidadtest/UniversidadTest.java
@@ -5,9 +5,12 @@
  */
 package universidadtest;
 
+import accesoADatos.AlumnoData;
 import accesoADatos.Conexion;
 import accesoADatos.MateriaData;
+import accesoadatos.InscripcionData;
 import entidades.Alumno;
+import entidades.Inscripcion;
 import entidades.Materia;
 import java.sql.Connection;
 import java.time.LocalDate;
@@ -37,9 +40,59 @@ public class UniversidadTest {
 	a.guardarMateria(new Materia("Matematica", 1, true));
 	 */
 	//Modificar una materia - prueba de metodo modificarMateria
-	MateriaData a = new MateriaData();
-	Materia lengua = new Materia(3,"Literatura", 2, true);
-	a.modificarMateria(lengua);
+//	MateriaData a = new MateriaData();
+//	Materia lengua = new Materia(3,"Literatura", 2, true);
+//	a.modificarMateria(lengua);
+
+
+
+
+//********************************** I N S C R I P C I O N **********************************       
+//************************************** METODOS  CRUD **************************************
+//*************************************** C R E A T E ***************************************
+//                 *****************************************************
+
+//        AlumnoData ad = new AlumnoData();
+//        MateriaData md = new MateriaData();
+//        InscripcionData id = new InscripcionData();
+//
+//        Alumno alumno4 = ad.buscarAlumnoPorID(3);
+//        Materia materia4 = md.buscarMateriaPorID(1);
+//        Inscripcion inscripcion = new Inscripcion(alumno4, materia4, 9.5);                //Se respetó el esquema de BD del PDF tiene nota como INT
+//        id.guardarInscripcion(inscripcion);
+
+
+//***************************************   R E A D   ***************************************
+//***************************************   LISTAR   **************************************** 
+
+    
+//        InscripcionData id = new InscripcionData();
+//
+//        for(Inscripcion inscripcion:id.obtenerInscripciones()){
+//             System.out.println("ID " + inscripcion.getIdInscripcion());
+//             System.out.println("Apellido "+inscripcion.getAlumno().getApellido());
+//             System.out.println("Materia " + inscripcion.getMateria().getNombre());
+//        }
+
+//                                    LISTAR POR ALUMNO 
+    
+//        InscripcionData id = new InscripcionData();
+//
+//        for(Inscripcion inscripcion:id.obtenerInscripcionesPorAlumno(1)){
+//             System.out.println("ID " + inscripcion.getIdInscripcion());
+//             System.out.println("Apellido "+inscripcion.getAlumno().getApellido());
+//             System.out.println("Materia " + inscripcion.getMateria().getNombre());
+//        }
+//        
+
+//*************************************** U P D A T E ***************************************
+
+//        InscripcionData id = new InscripcionData();
+//        id.actualizarNota(0, 3, 10);
+
+//*************************************** D E L E T E ***************************************
+//        InscripcionData id = new InscripcionData();
+//        id.borrarInscripcionMateriaAlumno(3, 10);
 
     }
 }
